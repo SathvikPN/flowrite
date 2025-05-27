da444a6d5c723633b244c165979d291215f14055
Sathvik PN <sathvikapn@gmail.com>
2025-05-27 11:37:59 +0530

    WIP: write page

--- app.py
+++ app.py
@@ -11,21 +11,31 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-@app.route('/')
-def index():
-    with open('content/index_article.md', 'r', encoding='utf-8') as file:
-        index_article = file.read()
-
-    article_html = markdown.markdown(index_article)
-    return render_template('index.html', data={
-        "title": "Home",
-        "message": "This is a simple Flask application with logging."
-    }, article_html=article_html)
-
 @app.route('/health')
 def health():
     logger.info("got ping for health check")
     return jsonify({"status": "ok"})
+
+
+@app.route('/')
+def index():
+    with open('content/index_article.md', 'r', encoding='utf-8') as file:
+        index_article = file.read()
+    article_html = markdown.markdown(index_article)
+    return render_template('index.html', data = {
+        "title": "Home",
+        "message": "Hompe Page for Flowrite",
+    }, article_html=article_html)
+
+@app.route('/write')
+def write():
+    return render_template('write.html', data = {
+        "title": "Write",
+        "message": "Write Page for Flowrite",
+    }, user = {
+        "is_authenticated": False,
+    })
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='127.0.0.1')
